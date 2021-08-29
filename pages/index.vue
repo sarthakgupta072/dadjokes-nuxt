738130b83d538299e6da50422fa60e728c9aa8f1
Sarthak Gupta <sarthakgupta072@gmail.com>
2021-08-29 09:54:05 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>
+      Welcome to the best corny dad jokes app on the web
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'Welcome to Dad Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for corny dad jokes',
+                }
+            ]
+        };
+    }
+};
+</script>
